ee4b5b86b5ea982613882c4a0c50482d873693fd
EricKogut <eric.jkogut@gmail.com>
2020-12-05 15:43:02 -0500

    Adding angular frontend
    - updated backend and git ignore
    - declared new angular project

--- backend/app.js
+++ backend/app.js
@@ -3,16 +3,78 @@
 var app = require('express')();
 var http = require('http').createServer(app);
 const express = require('express')
+var io = require('socket.io')(http);
 var cors = require('cors')
+
+//Var to store that chat sessions
+const chats = {};
+
+
 //Import for the dotenv file
 require('dotenv').config();
 
 app.use(express.json())
 app.use(cors())
 app.use(express.urlencoded({extended: true}));
+
 //////////////////////////////////
 //use npm start to start the app//
 
+
+//Defining what io can do
+
+///EXPLANATIONS
+
+////.on
+//This is an event listener where the first param is the name of the event
+//Second is the callback that is called when the function is executed  
+
+
+
+//Connection is a reserved event type!
+//We can create a callback to handle communication with one socket or used to broadcast to multiple sockets
+io.on("connection", socket => {
+    let previousId;
+
+    //Handling leaving and joining rooms
+    const safeJoin = currentId => {
+
+    //Making sure you can only join one room at a time
+      socket.leave(previousId);
+      socket.join(currentId);
+
+      //Now the previous id is what the currentid was
+      previousId = currentId;
+    };
+  
+    //When the client emits the getChat
+    //
+    socket.on("getchat", chatId => {
+      safeJoin(chatId);
+      socket.emit("chatument", chats[chatId]);
+    });
+  
+    socket.on("addchat", chat => {
+      chats[chat.id] = chat;
+      safeJoin(chat.id);
+      io.emit("chat", Object.keys(chats));
+      socket.emit("chat", chat);
+    });
+  
+    socket.on("editchat", chat => {
+      chats[chat.id] = chat;
+      socket.to(chat.id).emit("chatument", chat);
+    });
+  
+    io.emit("chats", Object.keys(chats));
+  });
+
+
+
+
+
+
+//Routes
 app.get('/hello', (req, res) => {
   res.send('<h1>Hello world</h1>');
 });
