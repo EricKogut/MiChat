9fefc19cc2c1c04ba6368edefa8855f91cd40948
EricKogut <eric.jkogut@gmail.com>
2020-12-06 17:16:31 -0500

    Updating ports

--- frontend/angularFrontend/environment.ts
+++ frontend/angularFrontend/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   backend_url: "http://localhost:5000",
-  socket_url: "http://localhost:5001"
+  socket_url: "http://ec2-18-208-119-230.compute-1.amazonaws.com:5000"
 };
